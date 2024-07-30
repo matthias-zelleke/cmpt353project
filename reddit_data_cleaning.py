a6ed8f8c98b1aa9186d3c32a26608abb12c61caa
Matthias Zelleke <mza151@sfu.ca>
2024-07-30 15:54:27 -0700

    remove deleted submissions/comments

--- reddit_data_cleaning.py
+++ reddit_data_cleaning.py
@@ -20,3 +20,23 @@ submissions_schema = types.StructType([
     types.StructField('year', types.IntegerType()),
     types.StructField('month', types.IntegerType()),
 ])
+
+def main(inputs):
+    
+    reddit_submissions_data = spark.read.json(inputs + '/submissions', schema=submissions_schema)
+    reddit_comments_data = spark.read.json(inputs + '/comments', schema=comments_schema)
+    
+    remove_text = ['', '[deleted', '[removed]']
+    
+    reddit_submissions_data = reddit_submissions_data.where(~functions.col('selftext').isin(remove_text))
+    reddit_comments_data = reddit_comments_data.where(~functions.col('body').isin(remove_text))
+    
+    reddit_submissions_data.write.json(inputs + '/submissions', mode='overwrite', compression='gzip')
+    reddit_comments_data.write.json(inputs + '/comments', mode='overwrite', compression='gzip')
+    
+    
+inputs = sys.argv[1]
+assert spark.version >= '3.5' # make sure we have Spark 3.5+
+spark.sparkContext.setLogLevel('WARN')
+
+main(inputs)
