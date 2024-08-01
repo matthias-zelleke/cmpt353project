4530705841a6d65c8c0c14a70c67a5611646e8b9
Matthias Zelleke <mza151@sfu.ca>
2024-08-01 00:50:33 -0700

    removing empty submissions/comments and stopwords

--- reddit_data_cleaning.py
+++ reddit_data_cleaning.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession, functions, types, Row
+import contractions
 
 spark = SparkSession.builder.appName('reddit extracter').config("spark.ui.showConsoleProgress", "true").getOrCreate()
 
@@ -21,15 +22,45 @@ submissions_schema = types.StructType([
     types.StructField('month', types.IntegerType()),
 ])
 
+
+def lowercase(string):
+    return string.lower()
+
+lowercase_udf = functions.udf(lowercase, types.StringType())
+
+def fix_contractions(string):
+    return contractions.fix(string)
+
+fix_contractions_udf = functions.udf(fix_contractions, types.StringType())
+
+
 def main(inputs):
     
     reddit_submissions_data = spark.read.json(inputs + '/submissions', schema=submissions_schema)
     reddit_comments_data = spark.read.json(inputs + '/comments', schema=comments_schema)
     
-    remove_text = ['', '[deleted', '[removed]']
+    # Remove submissions/comments with no text
+    
+    remove_text = ['', '[deleted]', '[removed]']
     
     reddit_submissions_data = reddit_submissions_data.where(~functions.col('selftext').isin(remove_text))
     reddit_comments_data = reddit_comments_data.where(~functions.col('body').isin(remove_text))
+    
+    # Remove contractions (e.g. don't -> do not, I'm -> I am)
+    
+    reddit_submissions_data = reddit_submissions_data.withColumn('selftext_no_contraction',
+                                    fix_contractions_udf(reddit_submissions_data['selftext'])) \
+                                    .drop('selftext')
+                                    
+    reddit_submissions_data = reddit_submissions_data.withColumnRenamed('selftext_no_contraction',
+                                                                        'selftext')
+    
+    reddit_comments_data = reddit_comments_data.withColumn('body_no_contraction',
+                                    fix_contractions_udf(reddit_comments_data['body'])) \
+                                    .drop('body')
+                                    
+    reddit_comments_data = reddit_comments_data.withColumnRenamed('body_no_contraction',
+                                                                  'body')
     
     reddit_submissions_data.write.json(inputs + '/submissions', mode='overwrite', compression='gzip')
     reddit_comments_data.write.json(inputs + '/comments', mode='overwrite', compression='gzip')
